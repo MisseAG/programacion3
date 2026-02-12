bae165a30ec4bafee11a3a5fc0868ed108831815
MisseAG <augusto.misseg@uqvirtual.edu.co>
2026-02-12 17:18:28 -0500

    Agrega un módulo Util con la función mostrar_mensaje para mejorar la modularidad del código

--- Talleres/problema-01/saludo.exs
+++ Talleres/problema-01/saludo.exs
@@ -3,6 +3,12 @@ defmodule Mensaje do
     "Bienvenidos a la empresa Once Ltda."
     |>IO.puts()
   end
+
+  defp mostrar_mensaje(mensaje) do
+    mensaje
+    |>IO.puts()
+  end
+
 end
 
 Mensaje.main()
