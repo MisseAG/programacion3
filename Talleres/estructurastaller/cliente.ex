f8b48c5c521f6479ea14c788c19b76705eede02b
MisseAG <augusto.misseg@uqvirtual.edu.co>
2026-04-21 15:53:06 -0500

    Entrega 21/04/2026

--- Talleres/estructurastaller/cliente.ex
+++ Talleres/estructurastaller/cliente.ex
@@ -46,4 +46,63 @@ defmodule Cliente do
         nueva_lista
     end
   end
+
+  def generar_mensaje(cliente) do
+    altura = cliente.altura |> Float.round(2)
+    "Hola #{cliente.nombre}, tu edad es de #{cliente.edad} años y " <> "tienes una altura de #{altura}\n"
+  end
+
+  def generar_mensaje_clientes(lista_clientes, parser) do
+    lista_clientes
+    |> Enum.map(parser)
+    |> Enum.join("\n")
+  end
+  # parsers
+  # &generar_mensaje/1
+  # &convertir_cliente_linea_csv/1
+
+  def crear(nombre, edad, altura) do
+    %Cliente{nombre: nombre, edad: edad, altura: altura}
+  end
+
+  def escribir_csv(clientes, nombre) do
+    clientes
+    |>generar_mensaje_clientes(&convertir_cliente_linea_csv/1)
+    |> (fn datos ->"nombre,edad,altura\n"<> datos end).()
+    |>(&File.write(nombre, &1)).()
+  end
+
+  def escribir_csv_persistente(clientes, nombre) do
+    header = "nombre,edad,altura\n"
+    ya_existe = File.exists?(nombre)
+
+    clientes
+    |> generar_mensaje_clientes(&convertir_cliente_linea_csv/1)
+    |> then(fn datos ->
+      if ya_existe, do: datos, else: header <> datos end)
+    |> then(&File.write(nombre, &1, [:append]))
+  end
+
+  defp convertir_cliente_linea_csv(cliente) do
+    "\n#{cliente.nombre}, #{cliente.edad}, #{cliente.altura}"
+  end
+
+  def leer_csv(nombre) do
+    nombre
+    |> File.stream!()
+    |> Stream.drop(1) #ignorar los encabezados
+    |> Enum.map(&convertir_cadena_cliente/1)
+  end
+
+  defp convertir_cadena_cliente(cadena) do
+    [nombre, edad, altura] =
+      cadena
+      |>String.split(",")
+      |> Enum.map(&String.trim/1)
+
+      edad = edad |> String.to_integer()
+      altura = altura |> String.to_float()
+
+      Cliente.crear(nombre, edad, altura)
+  end
 end
